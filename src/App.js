104941794b8e6b2c2cef0da1dd746fe758d2d253
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-08-25 09:37:11 -0700

    about done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import About from './components/About'
 
 function App() {
   return (
     <div>
       <NavBar />
       <Header />
+      <About />
       <Footer />
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import image from "../images/hpengland.jpg";
+import Typed from 'react-typed'
+
+export default function About() {
+  return (
+    <div className="about-main">
+      <div className="container">
+        <h2 className="about-main-title">About Me!
+        <img className="about-img" width="200px" height="260px" src={image} alt="kaleb"></img>
+        </h2>
+        <div className="about-main-info">
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p className="about-text">My name is Kaleb Muse, and I am a Full Stack Developer</p>
+          <p className="about-text">After working several different jobs, I decided to make a career change and attempt some coding!</p>
+          <p className="about-text">I have a certificate from the University of Washington in Full Stack Development.</p>
+          <p className="about-text">I love to play board games, video games, and dream of traveling the world.</p>
+          </div>
+      </div>
+    </div>
+  );
+}
