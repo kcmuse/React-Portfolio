830888715e9f4e21d2fdf228c8a9bec5d3b005cf
Kaleb Muse <kcmuse@Kalebs-MacBook-Air.local>
2021-08-24 17:48:08 -0700

    header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
-    <NavBar />
+    <div>
+      <NavBar />
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Typed from 'react-typed';
+
+export default function Header() {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>Welcome to my Portfolio!</h1>
+                    <Typed 
+                    className="typed-text"
+                    strings={["Web Design", "Full Stack Developer", "Student"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                    />
+                    <a href="#" className="btn-main">Contact Me</a>
+            </div>
+        </div>
+    )
+}
+
+
